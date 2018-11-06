aaed86a1a6efa76a8e2fa03771935131d8a242c1
Acastalope <acastalope@gmail.com>
2018-11-05 21:32:42 -0500

    initial commit

---
+++ JS.js
@@ -0,0 +1,39 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+		<title>CapstoneWk4ShoppingList</title>
+</head>
+function addItem() {
+  // Step 1: find the element
+var prodEl = document.getElementById("cheese");
+
+<p id="demo"></p>
+
+var products = ["cheese", "milk", "eggs"];
+var price = ["$6.99", "$2.99", "$1.99"]
+for (var i = 0; i < products.length; i++) {
+   
+var cheese = document.getElementById("addCheese");  
+var milk = document.getElementById("addMilk");
+var eggs = document.getElementById("addEggs");
+     
+
+   cheese.onclick= function() {
+     products.push("cheese");
+     console.log(products);
+   }
+
+
+var addItem = document.getElementById("addCheese");
+addItem.onclick = function() {
+    products.push("cheese");
+}
+    
+    
+var addItem = document.getElementById("addMilk");
+    addItem.onclick = function() {
+        products.push("milk");
+
+var newElement= 
+   
+  document.getElementById("products").appendchild(document.createElement("p"));
+  newElement.innerhtml= products[i]}
+
